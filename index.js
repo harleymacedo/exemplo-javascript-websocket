de83737417a4c97bdfac7c1149adedcc42c1ddd0
harleymacedo <harley.ip@gmail.com>
2022-05-19 10:32:59 -0300

    Funcionando com código simplificado

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+
+const http = require('http')
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -8,11 +11,11 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('mensageiro', (msg) => {
+    io.emit('mensageiro', msg);
   });
 });
 
-http.listen(port, () => {
+server.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}/`);
 });
