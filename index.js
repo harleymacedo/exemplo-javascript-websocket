74507a8a16a3453802872f4cd7a3a8c84379a7b3
harleymacedo <harley.ip@gmail.com>
2022-05-19 08:27:43 -0300

    Chat funcionando normalmente

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const { Server } = require('socket.io');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-const serverHttp = http.createServer(app);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(3000);
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}/`);
+});
